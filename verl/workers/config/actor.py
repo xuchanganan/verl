0b109171ba317aae7c1ae6ff394a67a58c26b790
xuchanganan <xuchanganan@163.com>
2026-01-09 21:31:30 +0800

    feat:update sum

--- verl/workers/config/actor.py
+++ verl/workers/config/actor.py
@@ -143,6 +143,7 @@ class ActorConfig(BaseConfig):
             "seq-mean-token-sum",
             "seq-mean-token-mean",
             "seq-mean-token-sum-norm",
+            "sum",
         ]
         if self.loss_agg_mode not in valid_loss_agg_modes:
             raise ValueError(f"Invalid loss_agg_mode: {self.loss_agg_mode}")
